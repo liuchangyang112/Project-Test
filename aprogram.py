d8f8530d258f4c0bbfa130e23071860c026ee792
Liu Chang Yang <liuchangyang@pop-os.localdomain>
2018-12-14 22:15:59 +0800

    Changed to a function

--- aprogram.py
+++ aprogram.py
@@ -1,3 +1,7 @@
-a = input("input a sentence")
-for i in range(0, 5):
-	print(a)
+def printtimes (a, i):
+	for x in range(0, i):
+		print(a)
+
+q1 = str(input("Input a sentence"))
+q2 = input("Input number of times")
+printtimes(q1, q2)
